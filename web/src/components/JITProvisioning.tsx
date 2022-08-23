77980b5a639a221c550db00bf42f370e8dbd2bbc
JinJis <david.jeong0724@gmail.com>
2022-08-24 00:20:58 +0900

    Finalize

--- web/src/components/JITProvisioning.tsx
+++ web/src/components/JITProvisioning.tsx
@@ -8,7 +8,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 const Step1 = () => {
   const codeString = 'mkdir openssl && cd openssl';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -22,7 +22,7 @@ const Step2 = () => {
     '            -subj "/CN=jjinj.amazon.com/C=US/L=San Fransisco" \\\n' +
     '            -keyout rootCA.key -out rootCA.crt';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -47,7 +47,7 @@ const Step3 = () => {
     'organizationName_default = JinJ\n' +
     'EOF';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -57,23 +57,23 @@ const Step4 = () => {
   const codeString = 'openssl req -new -sha256 -key rootCA.key -nodes -out rootCA.csr -config rootCA_openssl.conf\n' +
     'openssl x509 -req -days 3650 -extfile rootCA_openssl.conf -extensions v3_ca -in rootCA.csr -signkey rootCA.key -out rootCA.pem';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
 };
 
 const Step5 = () => {
-  const codeString = '# Get the registration code for the use below: \n' +
+  const codeString = '// Get the registration code for the use below: \n' +
     'aws iot get-registration-code \n' +
     '\n' +
-    '# Put the registration code in Common Name field\n' +
+    '// Put the registration code in Common Name field\n' +
     'openssl genrsa -out verificationCert.key 2048\n' +
     'openssl req -new -key verificationCert.key -out verificationCert.csr\n' +
     '\n' +
     'openssl x509 -req -in verificationCert.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out verificationCert.pem -days 500 -sha256';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -84,7 +84,7 @@ const Step6 = () => {
     '    --ca-certificate file://rootCA.pem \\\n' +
     '    --verification-cert file://verificationCert.pem';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -95,7 +95,7 @@ const Step7 = () => {
     '    --certificate-id certificateId \\\n' +
     '    --new-status ACTIVE';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -106,7 +106,7 @@ const Step8 = () => {
     'openssl req -new -key verificationCert.key -out jitpTestDevice.csr\n' +
     'openssl x509 -req -in jitpTestDevice.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out jitpTestDevice.pem -days 500 -sha256';
   return (
-    <SyntaxHighlighter language="python">
+    <SyntaxHighlighter language="shell">
       {codeString}
     </SyntaxHighlighter>
   );
@@ -116,16 +116,16 @@ class JITProvisioningPage extends Component {
   render() {
     return <Container header={<Header variant="h2">JIT Provisioning</Header>}>
       <SpaceBetween direction="vertical" size="s">
-        <h4>Create self-signed CA</h4>
+        <h3>Create self-signed CA</h3>
         <Step1/>
         <Step2/>
         <Step3/>
         <Step4/>
-        <h4>Register self-signed CA to AWS IoT Core</h4>
+        <h3>Register self-signed CA to AWS IoT Core</h3>
         <Step5/>
         <Step6/>
         <Step7/>
-        <h4>Generate X.509 certificate and connect</h4>
+        <h3>Generate X.509 certificate and connect</h3>
         <Step8/>
       </SpaceBetween>
     </Container>
--- web/src/components/kvsIntegration.tsx
+++ web/src/components/kvsIntegration.tsx
@@ -4,22 +4,85 @@ import SpaceBetween from "@awsui/components-react/space-between";
 import Button from "@awsui/components-react/button"
 import React, {Component} from 'react';
 
-class KVSIntegrationPage extends Component {
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+const Step1 = () => {
+  const codeString = 'aws --profile default  iot describe-endpoint --endpoint-type iot:CredentialProvider --output text > iot-credential-provider.txt';
+  return (
+    <SyntaxHighlighter language="shell">
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+
+const Step2 = () => {
+  const codeString = 'IOT_GET_CREDENTIAL_ENDPOINT=`cat iot-credential-provider.txt`'
+  return (
+    <SyntaxHighlighter language="shell">
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+
+const Step3 = () => {
+  const codeString = 'curl --silent -H "x-amzn-iot-thingname:kvs_example_camera_stream" --cert certificate.pem --key private.pem.key https://$IOT_GET_CREDENTIAL_ENDPOINT/role-aliases/KvsCameraIoTRoleAlias/credentials --cacert ./cacert.pem > token.json'
+  return (
+    <SyntaxHighlighter language="shell">
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+
+const Step4 = () => {
+  const codeString = 'sudo docker run -it --network="host" -v /Users/jjinj/repositories/kvs:/opt/certificate 546150905175.dkr.ecr.us-west-2.amazonaws.com/kinesis-video-producer-sdk-cpp-amazon-linux /bin/bash'
+  return (
+    <SyntaxHighlighter language="shell">
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+
+const Step5 = () => {
+  const codeString = 'export AWS_ACCESS_KEY_ID=\n' +
+    'export AWS_SECRET_ACCESS_KEY=\n' +
+    'export AWS_SESSION_TOKEN='
+  return (
+    <SyntaxHighlighter language="shell">
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+
+const Step6 = () => {
+  const codeString = 'gst-launch-1.0 -v rtspsrc location=rtsp://rtsp.stream/pattern short-header=TRUE ! rtph264depay ! h264parse ! kvssink iot-certificate="iot-certificate,endpoint=azogl4y0hhyqi-ats.credentials.iot.ap-northeast-2.amazonaws.com,ca-path=/opt/certificate/cacert.pem,cert-path=/opt/certificate/certificate.pem,key-path=/opt/certificate/private.pem.key,role-aliases=KvsCameraIoTRoleAlias" aws-region="ap-northeast-2" stream-name=kvs_example_camera_stream2'
+  return (
+    <SyntaxHighlighter language="shell">
+      {codeString}
+    </SyntaxHighlighter>
+  );
+};
+
+
+class KvsIntegrationPage extends Component {
   render() {
-    return <Container header={<Header variant="h2">Buttons</Header>}>
-      <SpaceBetween direction="vertical" size="l">
-        <Header variant="h3">Primary button</Header>
-        <Button variant="primary">Primary button</Button>
-        <Header variant="h3">Normal button</Header>
-        <Button variant="normal">Normal button</Button>
-        <Header variant="h3">Link button</Header>
-        <Button variant="link" iconName="external" target="_blank" href="https://google.com">Link button to
-          Google</Button>
-        <Header variant="h3">Icon button</Header>
-        <Button variant="icon" iconName="settings"/>
+    return <Container header={<Header variant="h2">KVS Integration</Header>}>
+      <SpaceBetween direction="vertical" size="s">
+        <h3>Setup Policy, Assumed Role through below link</h3>
+        <Button variant="link" iconName="external" target="_blank" href="https://docs.aws.amazon.com/kinesisvideostreams/latest/dg/how-iot.html#how-iot-thingnamestreamname">Link button to
+          Guideline</Button>
+        <h3>Configure AWS IoT Credential endpoint</h3>
+        <Step1/>
+        <h3>Get temporary IAM Token with X.509 certificate and Assumed role</h3>
+        <Step2/>
+        <Step3/>
+        <h3>Run Kinesis Producer</h3>
+        <Step4 />
+        <Step5 />
+        <Step6 />
       </SpaceBetween>
     </Container>
   }
 }
 
-export default KVSIntegrationPage;
+export default KvsIntegrationPage;
